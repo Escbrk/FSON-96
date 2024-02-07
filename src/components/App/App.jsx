61ce3ad02b701f7ed8853d3530c17016e403ddba
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-02-06 23:08:32 -0700

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,9 @@
 import "./App.css";
 import transactions from "../../payments.json";
-import { Payment } from "./Payment/Payment";
+import { PaymentList } from "./PaymentList/PaymentList";
 
 export const App = () => {
   return (
-    <ul style={{ listStyle: "none" }}>
-      {transactions.map(
-        ({
-          id,
-          cardNumber,
-          cardOwner,
-          cardType,
-          date,
-          amount,
-          description,
-          isPaid,
-        }) => {
-          return (
-            <li key={id}>
-              <Payment
-                cardNumber={cardNumber}
-                cardOwner={cardOwner}
-                cardType={cardType}
-                date={date}
-                amount={amount}
-                description={description}
-                isPaid={isPaid}
-              />
-            </li>
-          );
-        }
-      )}
-    </ul>
+    <PaymentList data={transactions} />
   );
 };
