446f5886c4666756c65549b4e2817e322bbaa226
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-02-23 20:42:23 -0700

    added validation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import UserForm from "../UserForm/UserForm";
 import "./App.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 //!============================
 export default function App() {
   return (
     <div>
+      <h1>Forms with Formik</h1>
       <UserForm />
     </div>
   );
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,45 +1,117 @@
 import { useId } from "react";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-export default function UserForm({ onSubmit }) {
+import css from "./UserForm.module.css";
+
+const UserSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too Short!")
+    .max(10, "Too Long!")
+    .required("Required!"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  role: Yup.string().oneOf(["guest", "user", "admin"]).required("Required"),
+});
+
+export default function UserForm() {
   const usernameFieldId = useId();
   const emeilFieldId = useId();
   const roleFieldId = useId();
-
-  const submit = (values, actions) => {
-    console.log(values);
-
-    actions.resetForm();
-  };
 
   return (
     <Formik
       initialValues={{
         username: "",
         email: "",
+        role: "guest",
       }}
-      onSubmit={submit}
+      validationSchema={UserSchema}
+      onSubmit={(values, actions) => {
+        console.log(values);
+
+        localStorage.setItem("data", JSON.stringify(values));
+
+        actions.resetForm();
+      }}
     >
-      <Form>
+      {/* {(props) => {
+        console.log(props);
+        return (
+          <Form className={css.form}>
+            <div>
+              <label htmlFor={usernameFieldId}>Username</label>
+              <br />
+              <Field name="username" id={usernameFieldId} />
+              <br />
+              <ErrorMessage
+                name="username"
+                component="span"
+                className={css.error}
+              />
+              <br />
+              <label htmlFor={emeilFieldId}>Email</label>
+              <br />
+              <Field type="email" name="email" id={emeilFieldId} />
+              <br />
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={css.error}
+              />
+            </div>
+
+            <div>
+              <label htmlFor={roleFieldId}>Role:</label>
+              <br />
+              <Field as="select" name="role" id={roleFieldId}>
+              <option value="admin">Admin</option>
+              <option value="user">User</option>
+              <option value="guest">Guest</option>
+              </Field>
+              <br />
+              <ErrorMessage
+                name="role"
+                component="span"
+                className={css.error}
+              />
+            </div>
+            <button type="submit" name="submit">
+              Submit
+            </button>
+          </Form>
+        );
+      }} */}
+
+      <Form className={css.form}>
         <div>
           <label htmlFor={usernameFieldId}>Username</label>
           <br />
           <Field name="username" id={usernameFieldId} />
           <br />
+          <ErrorMessage
+            name="username"
+            component="span"
+            className={css.error}
+          />
+          <br />
           <label htmlFor={emeilFieldId}>Email</label>
           <br />
           <Field type="email" name="email" id={emeilFieldId} />
+          <br />
+          <ErrorMessage name="email" component="span" className={css.error} />
         </div>
 
-        {/* <div>
+        <div>
           <label htmlFor={roleFieldId}>Role:</label>
           <br />
-          <select name="role" id={roleFieldId}>
-            <option value="guest">Guest</option>
-            <option value="user">User</option>
+          <Field as="select" name="role" id={roleFieldId}>
             <option value="admin">Admin</option>
-          </select>
-        </div> */}
+            <option value="user">User</option>
+            <option value="guest">Guest</option>
+          </Field>
+          <br />
+          <ErrorMessage name="role" component="span" className={css.error} />
+        </div>
         <button type="submit" name="submit">
           Submit
         </button>
