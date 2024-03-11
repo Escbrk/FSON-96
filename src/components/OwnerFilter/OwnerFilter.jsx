4447b14c173588121cf27b179d0ae60a26cae18c
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-03-10 20:17:52 -0600

    upd

---
+++ src/components/OwnerFilter/OwnerFilter.jsx
@@ -0,0 +1,23 @@
+import { useSearchParams } from "react-router-dom";
+
+const OwnerFilter = () => {
+  const [params, setParams] = useSearchParams();
+  const value = params.get("owner") ?? "";
+
+  const changeFilter = (newFilter) => {
+    params.set("owner", newFilter);
+    setParams(params);
+  };
+  return (
+    <div>
+      <b>Filter by owner</b>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => changeFilter(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default OwnerFilter;
--- src/pages/PaymentsPage.jsx
+++ src/pages/PaymentsPage.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PaymentList from "../components/PaymentList/PaymentList";
 import { getPayments } from "../../payments-api";
 import { useSearchParams } from "react-router-dom";
+import OwnerFilter from "../components/OwnerFilter/OwnerFilter";
 
 const PaymentsPage = () => {
   const [payments, setPayments] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const [params, setParams] = useSearchParams();
-  console.log(params)
+  const [params] = useSearchParams();
+  const ownerFilter = params.get("owner") ?? "";
 
   useEffect(() => {
     async function getData() {
@@ -27,14 +28,19 @@ const PaymentsPage = () => {
     getData();
   }, []);
 
+  const filteredPayments = useMemo(() => {
+    return payments.filter((payment) =>
+      payment.cardOwner.toLowerCase().includes(ownerFilter.toLowerCase())
+    );
+  }, [ownerFilter, payments]);
+
   return (
     <div>
       <h1>Payments Page!</h1>
-      <b>Filter by owner</b>
-      <input type="text" />
+      <OwnerFilter />
       {isLoading && <b>Loading payments...</b>}
       {error && <b>HTTP Error!</b>}
-      <PaymentList payments={payments} />
+      <PaymentList payments={filteredPayments} />
     </div>
   );
 };
