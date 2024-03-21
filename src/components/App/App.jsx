bd6db8a5e0ba85c202940553def1a70e6c9a1535
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-03-20 23:44:22 -0600

    create

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
 import "./App.css";
-import Navigation from "../Navigation/Navigation";
-
-const ClientInfo = lazy(() => import("../ClientInfo/ClientInfo"));
-const PaymentReceipt = lazy(() => import("../PaymentReceipt/PAymentReceipt"));
-
-const HomePage = lazy(() => import("../../pages/HomePage"));
-const PaymentsPage = lazy(() => import("../../pages/PaymentsPage"));
-const NotFoundPage = lazy(() => import("../../pages/NotFoundPage"));
-const PaymentsDetailsPage = lazy(() =>
-  import("../../pages/PaymentsDetailsPage")
-);
 
 const App = () => {
+
   return (
     <div>
-      <h1>React Router</h1>
-
-      <Navigation />
-
-      <Suspense fallback={null}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/payments" element={<PaymentsPage />} />
-          <Route path="/payments/:paymentId" element={<PaymentsDetailsPage />}>
-            <Route path="client" element={<ClientInfo />} />
-            <Route path="receipt" element={<PaymentReceipt />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
+      <h1>React Redux</h1>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++
@@ -1,3 +0,0 @@
-const HomePage = () => <h1>Welcome!</h1>;
-
-export default HomePage;
