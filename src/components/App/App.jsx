28690ecc72bb1f899032e1ce389671b25f6483a6
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-03-27 23:22:31 -0600

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,12 @@
-import { useEffect } from "react";
 import "./App.css";
-import { useDispatch } from "react-redux";
-import { fetchTasks } from "../../redux/tasksOps";
+
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchTasks());
-  }, [dispatch]);
+
 
   return (
     <div>
       <h1>HTTP requests with Redux</h1>
-      <h2>Hello</h2>
     </div>
   );
 };
